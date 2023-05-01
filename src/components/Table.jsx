d3a39d32a6fcd9b0aebf0f3f140abd5e74b5fb3a
bestiannn <rebolledocofrebastian@gmail.com>
2023-05-01 01:12:20 -0400

    feat: basic gameplay in realtime added

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { channel } from '../services/supabase';
-import useUser from "../global/user";
+import { useUser, useGame } from "../global";
 
 const Table = () => {
-    const [gameState, setGameState] = useState(Array(9).fill(''));
     const { userFigure } = useUser();
+    const { board, setBoard, turn, setTurn } = useGame();
 
-    const handleClick = (index) => {
-        const newGameState = [...gameState];
-        newGameState[index] = userFigure;
-        setGameState(newGameState);
-        channel.send({
+    const handleClick = async (index) => {
+        if (turn !== userFigure) return;
+        const newBoard = [...board];
+        newBoard[index] = userFigure;
+        setBoard(newBoard);
+        await channel.send({
             type: 'broadcast',
             event: 'message',
             payload: {
@@ -23,18 +24,17 @@ const Table = () => {
     useEffect(() => {
         channel.on('broadcast', { event: 'message' }, ({ payload }) => {
             const { index, figure } = payload;
-            console.log(payload);
-            setGameState((gameState) => {
-                const newGameState = [...gameState];
-                newGameState[index] = figure;
-                return newGameState;
-            });
+            const newBoard = board
+            newBoard[index] = figure;
+            setBoard(newBoard);
+            console.log(turn)
+            setTurn(figure === 'X' ? 'O' : 'X');
         }).subscribe()
     }, []);
 
     return (
         <div className="w-full text-center flex-1 grid grid-cols-3 grid-rows-3 gap-1 bg-black">
-            {gameState.map((figure, index) => {
+            {board.map((figure, index) => {
                 return (
                     <div key={index}>
                         {!figure && <div className="w-full h-full cursor-pointer grid place-content-center bg-white" onClick={() => handleClick(index)}></div>}
@@ -42,8 +42,7 @@ const Table = () => {
                         {figure === 'O' && <i className="nes-icon heart w-full h-full grid place-content-center bg-white"></i>}
                     </div>
                 )
-            })
-            }
+            })}
         </div>
     )
 }
